c91fe44e0b2df992296ad24872352a34fed7e7d4
rastating <rob@rastating.com>
2016-07-09 22:40:13 +0100

    Initial commit

---
+++ LogScanner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace OpenVPNMonitor
+{
+    class LogScanner
+    {
+        private const string CONNECTION_PATTERN = @"(?<CommonName>.+?),(?<RealAddress>(?:[0-9]{1,3}\.){3}[0-9]{1,3}):(?<Port>[0-9]+),(?<BytesReceived>.+?),(?<BytesSent>.+?),(?<ConnectedSince>.+)";
+
+        private Dictionary<string, List<IPAddress>> connections;
+        private string logFileName;
+
+        public delegate void NewConnectionHandler(string commonName, IPAddress realAddress);
+        public event NewConnectionHandler NewConnectionDetected;
+
+        public LogScanner(string logFileName)
+        {
+            this.logFileName = logFileName;
+            connections = new Dictionary<string, List<IPAddress>>();
+        }
+
+        public void Scan()
+        {
+            string data = File.ReadAllText(logFileName);
+            Regex regex = new Regex(CONNECTION_PATTERN);
+            MatchCollection matches = regex.Matches(data);
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                string commonName = matches[i].Groups["CommonName"].Value;
+                IPAddress realAddress = IPAddress.Parse(matches[i].Groups["RealAddress"].Value);
+
+                if (connections.Keys.Contains(commonName))
+                {
+                    List<IPAddress> addresses = connections[commonName];
+                    if (addresses.Count(ip => ip.Equals(realAddress)) == 0)
+                    {
+                        addresses.Add(realAddress);
+                        NewConnectionDetected?.Invoke(commonName, realAddress);
+                    }
+                }
+                else
+                {
+                    List<IPAddress> addresses = new List<IPAddress>();
+                    addresses.Add(realAddress);
+                    connections.Add(commonName, addresses);
+                    NewConnectionDetected?.Invoke(commonName, realAddress);
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Threading;
+
+namespace OpenVPNMonitor
+{
+    class Program
+    {
+        private static bool keepRunning = true;
+        private static AutoResetEvent waitHandle = new AutoResetEvent(false);
+        private static Properties.Settings settings = Properties.Settings.Default;
+        private static string pushoverUrl = "https://api.pushover.net/1/messages.json";
+
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            Console.WriteLine("Using log file: {0}", settings.LogFile);
+
+            LogScanner scanner = new LogScanner(settings.LogFile);
+            scanner.NewConnectionDetected += Scanner_NewConnectionDetected;
+            scanner.Scan();
+
+            System.Timers.Timer timer = new System.Timers.Timer(settings.ScanFrequency);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            while (keepRunning)
+            {
+                scanner.Scan();
+                waitHandle.WaitOne();
+            }
+
+            timer.Stop();
+        }
+
+        private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            waitHandle.Set();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            keepRunning = false;
+            waitHandle.Set();
+        }
+
+        private static void Scanner_NewConnectionDetected(string commonName, IPAddress realAddress)
+        {
+            WebClient client = new WebClient();
+            NameValueCollection values = new NameValueCollection();
+            string message = string.Format("Detected new connection by {0} from {1}", commonName, realAddress.ToString());
+
+            values.Add("token", settings.PushoverAppToken);
+            values.Add("user", settings.PushoverUserKey);
+            values.Add("message", message);
+
+            client.UploadValues(pushoverUrl, "POST", values);
+            Console.WriteLine(message);
+        }
+    }
+}
