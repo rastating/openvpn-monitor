44d1917e54357164d3af5b44fd9afbcb70a1edaa
rastating <rob@rastating.com>
2016-07-09 22:52:36 +0100

    Change to use FileSystemWatcher instead of polling

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Specialized;
+using System.IO;
 using System.Net;
 using System.Threading;
 
@@ -7,56 +8,58 @@ namespace OpenVPNMonitor
 {
     class Program
     {
-        private static bool keepRunning = true;
         private static AutoResetEvent waitHandle = new AutoResetEvent(false);
         private static Properties.Settings settings = Properties.Settings.Default;
         private static string pushoverUrl = "https://api.pushover.net/1/messages.json";
+        private static LogScanner scanner;
 
         static void Main(string[] args)
         {
             Console.CancelKeyPress += Console_CancelKeyPress;
             Console.WriteLine("Using log file: {0}", settings.LogFile);
 
-            LogScanner scanner = new LogScanner(settings.LogFile);
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = Path.GetDirectoryName(settings.LogFile);
+            watcher.Filter = Path.GetFileName(settings.LogFile);
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Changed += LogFile_Changed;
+            watcher.EnableRaisingEvents = true;
+
+            scanner = new LogScanner(settings.LogFile);
             scanner.NewConnectionDetected += Scanner_NewConnectionDetected;
             scanner.Scan();
 
-            System.Timers.Timer timer = new System.Timers.Timer(settings.ScanFrequency);
-            timer.Elapsed += Timer_Elapsed;
-            timer.Start();
+            waitHandle.WaitOne();
 
-            while (keepRunning)
-            {
-                scanner.Scan();
-                waitHandle.WaitOne();
-            }
-
-            timer.Stop();
+            watcher.EnableRaisingEvents = false;
+            watcher.Dispose();
         }
 
-        private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        private static void LogFile_Changed(object sender, FileSystemEventArgs e)
         {
-            waitHandle.Set();
+            scanner.Scan();
         }
 
         private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
         {
             e.Cancel = true;
-            keepRunning = false;
             waitHandle.Set();
         }
 
         private static void Scanner_NewConnectionDetected(string commonName, IPAddress realAddress)
         {
-            WebClient client = new WebClient();
-            NameValueCollection values = new NameValueCollection();
             string message = string.Format("Detected new connection by {0} from {1}", commonName, realAddress.ToString());
+            using (WebClient client = new WebClient())
+            {
+                NameValueCollection values = new NameValueCollection();
 
-            values.Add("token", settings.PushoverAppToken);
-            values.Add("user", settings.PushoverUserKey);
-            values.Add("message", message);
+                values.Add("token", settings.PushoverAppToken);
+                values.Add("user", settings.PushoverUserKey);
+                values.Add("message", message);
 
-            client.UploadValues(pushoverUrl, "POST", values);
+                client.UploadValues(pushoverUrl, "POST", values);
+            }
+
             Console.WriteLine(message);
         }
     }
